0b6a88e2ec0ce7670c91b571be29e1e3d8186244
Myat <myatthinkhaing1234@gmail.com>
2022-08-02 13:49:44 +0630

    Testing basic Java Sc

---
+++ basic2.js
@@ -0,0 +1,27 @@
+const flag = true
+
+if (!flag)
+{
+    console.log("Condition is Correct")
+}
+else{
+    console.log("Console is Incorrect")
+}
+
+let i=0
+while(i>10){
+    i++
+    console.log("i value is "+i)
+}
+
+do{
+    i++
+    console.log(i)
+}while(i<10);
+
+
+let required = true
+while(required){
+    console.log(required)
+    required = false
+}
