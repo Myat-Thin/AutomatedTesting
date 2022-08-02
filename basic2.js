8e42ea23779751715bdcd5de1b700d4309d20827
Myat <myatthinkhaing1234@gmail.com>
2022-08-02 14:27:39 +0630

    for loop lesson

--- basic2.js
+++ basic2.js
@@ -25,3 +25,17 @@ while(required){
     console.log(required)
     required = false
 }
+
+
+console.log("*************************")
+let n=0
+for (let k=1; k<=100 ; k++)
+{
+    if(k%2 == 0 && k%5==0){
+        n++
+        console.log(k)
+        if (n == 3)
+        break
+    }
+     
+}
